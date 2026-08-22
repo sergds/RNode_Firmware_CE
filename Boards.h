152a9a44ca64d7eb6ab325743a8a2e38ccc1cda5
Sergey Morozyuk <git@sergds.xyz>
2026-08-22 14:49:32 +0300

    Boards(rp2040-lora): this board lacks a tcxo. Disable

--- Boards.h
+++ Boards.h
@@ -1490,7 +1490,7 @@
                     // SX1262
           {
               false, // DEFAULT_SPI
-              true, // HAS_TCXO
+              false, // HAS_TCXO
               true  // DIO2_AS_RF_SWITCH
           }
       };
